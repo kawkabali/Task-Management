024d5d130556f972c6bd908fbb7ee1b4e2fd5692
kawkab ali <kawkabalshorman8@gmail.com>
2026-07-05 02:25:23 +0300

    Deploy backend on Vercel

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,10 @@ app.get("/", (req, res) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
